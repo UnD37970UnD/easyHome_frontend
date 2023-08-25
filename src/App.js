7bd60bd4dd80c28caba259959d57688272d723ec
Iustin Ivan <iustinivan2002@gmail.com>
2023-08-25 15:55:39 +0300

    adding each 3 articles on it's own row

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
-import ArticlesView from "./components/articleListView"
-import React, {useState, useEffect} from 'react';
+import React, {useState, useEffect, Suspense} from 'react';
 import axios from 'axios'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import Filters from './components/filters';
 
+const ArticlesView = React.lazy(() => import ("./components/articleListView"));
+
 
 function App() {
   const [ArticlesList, setArticlesList] = useState([{}])
+  const [noArticles, setNoArticles] = useState('')
   const [ApiUrl, setApiUrl] = useState("https://easyhome-1-y9546418.deta.app/api/v1/de_inchiriat/?from_price=0&to_price=99999999&skip=0&lenght=40")
   
   
@@ -15,7 +17,11 @@ function App() {
   useEffect(() => {
     axios.get(ApiUrl)
     .then(res => {
-      setArticlesList(res.data.data);
+      if (res.data.data === undefined || res.data.data.length == 0)
+        setNoArticles("empty");
+      else
+        setNoArticles("data");
+        setArticlesList(res.data.data);
     })
     .catch(error => {
       console.error('Error fetching articles:', error);
@@ -25,21 +31,28 @@ function App() {
   const ApplyFilters = (Url) => {
     setApiUrl(Url)
   };
-
+  function ToView(){
+    if(noArticles=="empty")
+    {
+        return(<h3>Nu am gasit imobiliarea aici</h3>)
+    }
+    else return (<ArticlesView ArticlesList={ArticlesList}/>)
+  }
   return (
-    <div className='App container'>
+    <div className='App container-xxl'>
       <h1 className='card text-white bg-dark mb-1'>easyHome</h1>
       <h6 className='card text-white bg-primary mb-3'> Find your new home</h6>
       <div className='row'>
-        <div className='col-1 justify-content-left align-items-center mx-auto' style={{"width":"300px", "marginTop":"15px"}}>
+        <div className='col-3 col-xs-12 justify-content-left align-items-center mx-auto' style={{"width":"300px", "marginTop":"15px"}}>
         <Filters ApplyFilters={ApplyFilters} />
         </div>
       
-        <div className='col-8 list-group-item justify-content-center align-items-center mx-auto' style={{"marginTop":"15px"}}>
+        <div className='col-9 col-xs-12 col-sm-fluid list-group-item justify-content-center align-items-center mx-auto' style={{"marginTop":"15px"}}>
           <h4 className='card text-white bg-dark mb-3'>Anunturi</h4>
-          <div>
-            <ArticlesView ArticlesList={ArticlesList}/> 
-          </div>
+            <Suspense fallback={<div>Loading...</div>}>
+              <ToView/>
+            
+            </Suspense>
         </div>
 
       </div>
--- src/components/articleListView.js
+++ src/components/articleListView.js
@@ -1,7 +1,28 @@
-import ArticleItem from "./article";
+import React from 'react';
+import ArticleItem from './article';
 
-export default function ArticlesView(props){
-    const listItems = props.ArticlesList.map((articol,index) => ArticleItem(articol,index));
-      return <ul>{listItems}</ul>;
+export default function ArticlesView(props) {
+  const { ArticlesList } = props;
 
+  const renderArticleRows = () => {
+    const rows = [];
+    let currentRow = [];
+
+    ArticlesList.forEach((article, index) => {
+      currentRow.push(ArticleItem(article,index));
+
+      if (currentRow.length === 3 || index === ArticlesList.length - 1) {
+        rows.push(
+          <div key={`row-${index}`} className="row align-items-center">
+            {currentRow}
+          </div>
+        );
+        currentRow = [];
+      }
+    });
+
+    return rows;
+  };
+
+  return <div className="container">{renderArticleRows()}</div>;
 }
